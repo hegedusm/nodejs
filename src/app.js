f28508afcaf01c5900c8551008723b7ccc8fc868
hegedusm <matehegedus1@gmail.com>
2019-09-21 10:53:12 +0200

    config and model classes, app import and log

--- src/app.js
+++ src/app.js
@@ -1,1 +1,6 @@
-console.log("Success");
+import { User, Product } from "./models";
+import config from "./config/config";
+
+console.log(config.name);
+new User();
+new Product();
---
+++ src/models/index.js
@@ -0,0 +1,2 @@
+export { default as User } from "./Product";
+export { default as Product } from "./User";
