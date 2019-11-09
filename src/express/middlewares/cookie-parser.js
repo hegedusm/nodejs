cefe49ebe25fb4394d725cc3bde9e422d490f651
hegedusm <matehegedus1@gmail.com>
2019-11-09 12:14:38 +0100

    cookie parser filters out empty cookie on request

--- src/express/middlewares/cookie-parser.js
+++ src/express/middlewares/cookie-parser.js
@@ -1,11 +1,13 @@
 
 const cookieParser = (req, res, next) => {
-	const rawCookies = (req.headers.cookie || "" ).split(";");
+	const rawCookies = (req.headers.cookie || "").split(";");
 	const parsedCookies = {};
-	rawCookies.map(cookie => cookie.trim()).forEach(cookie => {
-		const cookieParts = cookie.split("=");
-		parsedCookies[cookieParts[0]] = cookieParts[1];
-	});
+	rawCookies.map(cookie => cookie.trim())
+		.filter(cookie => !!cookie)
+		.forEach(cookie => {
+			const cookieParts = cookie.split("=");
+			parsedCookies[cookieParts[0]] = cookieParts[1];
+		});
 	req.parsedCookies = parsedCookies;
 	next();
 }
