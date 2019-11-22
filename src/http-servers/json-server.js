68f0b05218b9bc3e8bfcd7da2b8dfbf168ba46ac
hegedusm <matehegedus1@gmail.com>
2019-11-22 19:36:46 +0100

    formatted json

--- src/http-servers/json-server.js
+++ src/http-servers/json-server.js
@@ -5,6 +5,16 @@ http.createServer()
 		res.writeHead(200, {
 			"Content-Type": "application/json"
 		});
-		const product = { id: 1, name: 'Supreme T-Shirt', brand: 'Supreme', price: 99.99, options: [{ color: 'blue' }, { size: 'XL' }] };
+		const product = {
+			id: 1,
+			name: 'Supreme T-Shirt',
+			brand: 'Supreme',
+			price: 99.99,
+			options: [{
+				color: 'blue'
+			}, {
+				size: 'XL'
+			}]
+		};
 		res.end(JSON.stringify(product));
 	}).listen(3000);
