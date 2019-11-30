d095b0aa61e691497966f578b9b352e81d0ba693
hegedusm <matehegedus1@gmail.com>
2019-11-30 10:43:35 +0100

    sequelizer config, seeders, migrations and models updated controller functions to use database

--- src/express/controllers/users.js
+++ src/express/controllers/users.js
@@ -1,5 +1,5 @@
-const users = ["admin", "test"];
+import db from "../database/models";
 
 exports.list = (req, resp) => {
-	resp.json(users);
+	db.Users.findAll({raw:true, attributes: ['userName']}).then(users => resp.json(users));
 }
